e910c43cefdbdc249920360f50244527ac3926f9
Vlad Pop <61190539+NoAnswerss@users.noreply.github.com>
2022-05-12 21:45:56 +0100

    changed background

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("cards-el")
 function startGame() {
+    cardEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
