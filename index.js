97b40dd9abf861f112d8940fb62a39bfa459f601
Vlad Pop <61190539+NoAnswerss@users.noreply.github.com>
2022-05-19 21:38:26 +0100

    added math random to the deck

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 2
-let secondCard = 7
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard] // array - ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -10,12 +10,24 @@ let sumEl = document.getElementById("sum-el")
 let cardEl = document.getElementById("cards-el")
 let drawCard = document.getElementById("newcard-el")
 
+
+function getRandomCard() {
+    return 7}
+
+
 function startGame() {
     renderGame()
 }
+let randomNumber = Math.random()
+
+console.log(randomNumber)
 
 function renderGame() {
-    cardEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardEl.textContent += cards[i] + " "
+    } 
+    
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -29,11 +41,10 @@ function renderGame() {
     messageEl.textContent = message
 }
 function newCard() {
-    let drawnCard = 4
+    let drawnCard = getRandomCard()
     console.log("Drawing a new card from the deck!")
     sum += drawnCard
     cards.push(drawnCard)
     console.log(cards)
     startGame()
 }
-
